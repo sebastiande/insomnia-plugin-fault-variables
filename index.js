334219d9f9fcbac2f803fb5a30e6f0fed078ab0e
Sebastian Dernbach <sebastian.dernbach@ionos.com>
2024-06-28 16:32:09 +0200

    read projects to dropdown

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ function readFaultProperty(project, filepath, property) {
         return '[ERROR] Fault not mounted (Can not find fault in ~/.fault)!';
     }
 
+    if (project === '0') {
+        console.error('[insomnia-plugin-fault-variables] can not find fault projects');
+        return '[ERROR] can not find fault projects!';
+    }
+
     const projectPath = faultPath + project + '/';
     if (!fs.existsSync(projectPath)) {
         console.error('[insomnia-plugin-fault-variables] can not find project in: ' + projectPath);
@@ -34,24 +39,52 @@ function getFaultPath() {
     return defaultFaultPath.replaceAll('~', os.homedir());
 }
 
-module.exports.templateTags = [{
-    name: 'faultVariable',
-    displayName: 'Fault Variable',
-    description: 'Inserts the content of a property stored within fault',
-    args: [
+function getFaultProjects() {
+    const faultPath = getFaultPath();
+    if (!fs.existsSync(faultPath)) {
+        console.error('[insomnia-plugin-fault-variables] can not find fault directory, only searched within home directory ~/.fault/');
+        return [{
+            displayName: 'No fault projects found',
+            value: '0'
+        }]
+    }
+
+    const faultProjects = fs.readdirSync(faultPath, { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name);
+    let projectsEnum = [];
+    faultProjects.forEach(item => {
+        projectsEnum.push({
+            displayName: item,
+            value: item
+        });
+    })
+    return projectsEnum;
+}
+
+function getPropertyFiles(project) {
+    console.log(project);
+    return [{
+        displayName: 'no',
+        value: '0'
+    }];
+}
+
+function getArgs() {
+    const properties = (args => getPropertyFiles(args));
+    console.log(properties);
+    return [
         {
             displayName: 'Project',
             description: 'The fault project name.',
-            type: 'string',
-            defaultValue: '',
-            placeholder: 'com.ionos.myproject:service-name'
+            type: 'enum',
+            options: getFaultProjects()
         },
         {
             displayName: 'Filepath',
             description: 'The path to a file within the project',
-            type: 'string',
-            defaultValue: '',
-            placeholder: 'prod/.properties'
+            type: 'enum',
+            options: getPropertyFiles()
         },
         {
             displayName: 'Property',
@@ -60,7 +93,14 @@ module.exports.templateTags = [{
             defaultValue: '',
             placeholder: 'service.password'
         }
-    ],
+    ];
+}
+
+module.exports.templateTags = [{
+    name: 'faultVariable',
+    displayName: 'Fault Variable',
+    description: 'Inserts the content of a property stored within fault',
+    args: getArgs(),
     async run(context, project, filepath, property) {
         try {
             return readFaultProperty(project, filepath, property);
